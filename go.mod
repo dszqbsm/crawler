49749653d36c4613e34fc609a3714d7797399694
sjh <570209909@qq.com>
2025-01-04 23:27:39 +0800

    docs📝: 封装爬取页面功能函数，使用字符集库实现编码的通用性

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/dszqbsm/crawler
 
 go 1.23.0
+
+require (
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
--- mian.go
+++ mian.go
@@ -1,28 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"net/http"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 func main() {
 	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
-
-	if err != nil {
-		fmt.Printf("fetch url error:%v", err)
-		return
-	}
-
-	// 在当前goroutine结束前关闭资源
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
+	body, err := Fetch(url)
 
 	if err != nil {
 		fmt.Printf("read content failed:%v", err)
@@ -30,4 +22,36 @@ func main() {
 	}
 
 	fmt.Printf("body: %s", string(body))
+}
+
+func Fetch(url string) ([]byte, error) {
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		fmt.Printf("fetch error:%v", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
