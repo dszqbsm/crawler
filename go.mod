346e5123dc05db0e829dc08436cfa4525496aa86
sjh <570209909@qq.com>
2025-01-04 19:25:17 +0800

    tag📌: 爬取网页内容并输出

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dszqbsm/crawler
+
+go 1.23.0
---
+++ mian.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	url := "https://www.thepaper.cn/"
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Printf("fetch url error:%v", err)
+		return
+	}
+
+	// 在当前goroutine结束前关闭资源
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%v", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Printf("read content failed:%v", err)
+		return
+	}
+
+	fmt.Printf("body: %s", string(body))
+}
